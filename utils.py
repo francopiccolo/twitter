1c0f264a64553972e83e062a22f96f2e74e72288
Franco Piccolo <francopiccolo@gmail.com>
2021-11-27 23:58:25 +0000

    refactored make followers df to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import os
+from datetime import datetime, timezone
+import numpy as np
+import pandas as pd
 import tweepy
 
 def get_api():
@@ -30,3 +33,24 @@ def iterate_over_cursors(function, screen_name):
         data.extend(partial_data)
         cursor = cursors[1]
     return data
+
+def make_users_df(users, source_screen_name):
+    important_attributes = ['screen_name', 'location', 'followers_count', 'friends_count', 'created_at', 'favourites_count', 'statuses_count']
+    data = {k:[] for k in important_attributes}
+    data['last_status'] = []
+    for user in users:
+        attributes = user._json
+        for attribute in important_attributes:
+            data[attribute].append(attributes[attribute])
+        last_status = np.nan
+        if 'status' in attributes:
+            last_status = user.status.created_at
+        
+        data['last_status'].append(last_status)
+
+    users_df = pd.DataFrame(data)
+    users_df['friends_followers_ratio'] = users_df['friends_count'] / users_df['followers_count']
+    users_df['time_processed'] = datetime.now(timezone.utc)
+    users_df['days_since_last_status'] = (users_df['time_processed'] - users_df['last_status']).dt.days
+    users_df['source_screen_name'] = source_screen_name
+    return users_df
